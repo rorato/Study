f9a1038c9716f098158a1496e4fb49e03e1fc072
Raymond <rclark0073@twc.com>
2018-06-12 17:45:37 -0500

    Added Object functions

--- Ch-2 Objects.js
+++ Ch-2 Objects.js
@@ -1,5 +1,6 @@
 //Explore and practice objects
 
+function Object1(){
 var obj = {
     a: "hello world",
     b: 42,
@@ -9,3 +10,22 @@ var obj = {
 console.log(obj.a);
 console.log(obj.b);
 console.log(obj.c);
+}
+
+//----------------------------------------------------------
+
+function Object2(){
+    var obj = {
+        a: "Jerkin Gerkin",
+        b: 1337
+    };
+
+    var b = "Bullsnit";
+
+    console.log(obj[a]);
+    console.log(obj[b]);
+    console.log(obj['b']);
+}
+
+Object1();
+Object2();
