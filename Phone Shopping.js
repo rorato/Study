1fcae01ad675c4b7a46b933e6fd4d171e0b37282
Raymond <rclark0073@twc.com>
2018-06-12 17:18:40 -0500

    Used .toFixed on line 14 to fix Decimal Place for currency

--- Phone Shopping.js
+++ Phone Shopping.js
@@ -1,10 +1,10 @@
 //Phone buying algorithm
 
 function GoodsTax(amt){
-    const KY_TAX = .06;
+    const KY_TAX = .06; //Using the chrome console with this const causes no issues when the code is ran again and again
     return amt + (amt * KY_TAX);
 }
-const PHONE_PRICE = 198.99;
+//const PHONE_PRICE = 198.99; //When placed in a function, then CONST is ok with being reinducted
 var bankBalance = 1349.83;
 var phonesBought = 0;
 
@@ -12,4 +12,4 @@ for (i = bankBalance; i >= PHONE_PRICE; i = i - PHONE_PRICE){
     phonesBought++;
 }
 alert("You bought a total of " + phonesBought + "!");
-alert("Your total price including taxes is: " + GoodsTax(phonesBought * PHONE_PRICE));
+alert("Your total price including taxes is: $" + GoodsTax(phonesBought * PHONE_PRICE).toFixed(2));
