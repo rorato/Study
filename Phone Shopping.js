31b442c20d55c6939466e13906f9097e391fae59
Raymond <rclark0073@twc.com>
2018-06-12 17:28:27 -0500

    Changed Function name and return

--- Phone Shopping.js
+++ Phone Shopping.js
@@ -1,15 +1,18 @@
 //Phone buying algorithm
 
-function GoodsTax(amt){
+function Total(amt){
     const KY_TAX = .06; //Using the chrome console with this const causes no issues when the code is ran again and again
-    return amt + (amt * KY_TAX);
+    return "$" + (amt + (amt * KY_TAX)).toFixed(2);
 }
 //const PHONE_PRICE = 198.99; //When placed in a function, then CONST is ok with being reinducted
-var bankBalance = 1349.83;
+const ACCESSORY_PRICE = 4.99;
+var bankBalance = prompt("How mucha muney u hef: ");
 var phonesBought = 0;
+var accessoriesBought = 0;
 
 for (i = bankBalance; i >= PHONE_PRICE; i = i - PHONE_PRICE){
     phonesBought++;
+    accessoriesBought++;
 }
 alert("You bought a total of " + phonesBought + "!");
-alert("Your total price including taxes is: $" + GoodsTax(phonesBought * PHONE_PRICE).toFixed(2));
+alert("Your total price including taxes is: " + Total((phonesBought * PHONE_PRICE) + (accessoriesBought * ACCESSORY_PRICE)));
