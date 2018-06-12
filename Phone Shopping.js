210100fd82b474624c89f8412d1014ff47d2cdb6
Raymond <rclark0073@twc.com>
2018-06-12 17:14:17 -0500

    Added GoodsTax function and Another alert of Total

--- Phone Shopping.js
+++ Phone Shopping.js
@@ -1,10 +1,15 @@
 //Phone buying algorithm
 
+function GoodsTax(amt){
+    const KY_TAX = .06;
+    return amt + (amt * KY_TAX);
+}
 const PHONE_PRICE = 198.99;
 var bankBalance = 1349.83;
 var phonesBought = 0;
 
-for (i = bankBalance; i >= PHONE_PRICE; i=-PHONE_PRICE){
+for (i = bankBalance; i >= PHONE_PRICE; i = i - PHONE_PRICE){
     phonesBought++;
 }
 alert("You bought a total of " + phonesBought + "!");
+alert("Your total price including taxes is: " + GoodsTax(phonesBought * PHONE_PRICE));
