80079afa48ef2d58336da63cc6ae99c4de69c0a7
Raymond <rclark0073@twc.com>
2018-06-12 17:29:02 -0500

    Removed Comment on PHONE_PRICE

--- Phone Shopping.js
+++ Phone Shopping.js
@@ -4,7 +4,7 @@ function Total(amt){
     const KY_TAX = .06; //Using the chrome console with this const causes no issues when the code is ran again and again
     return "$" + (amt + (amt * KY_TAX)).toFixed(2);
 }
-//const PHONE_PRICE = 198.99; //When placed in a function, then CONST is ok with being reinducted
+const PHONE_PRICE = 198.99; //When placed in a function, then CONST is ok with being reinducted
 const ACCESSORY_PRICE = 4.99;
 var bankBalance = prompt("How mucha muney u hef: ");
 var phonesBought = 0;
