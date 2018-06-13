22bf929312a5599a618e962449a781e7883758a2
Raymond <rclark0073@twc.com>
2018-06-13 17:00:52 -0500

    Added another Object for a guessing game

--- Ch-2 Objects.js
+++ Ch-2 Objects.js
@@ -27,5 +27,41 @@ function Object2(){
     console.log(obj['b']);
 }
 
+function Object3(){
+    var obj = {
+        Polly: "Pocket",
+        b: "Not a Pocket"
+    };
+    
+    var a = "Polly";
+
+    console.log(obj['b']);
+    console.log(obj[a]);
+
+}
+
+function FunObject(){
+    var obj = {
+        Kunta: "You sir, are a Keentei",
+        Keentei: "Let us not have an argument now...",
+        Jack: "Not this guy Jake",
+        Pickle: "Pickle Riiiiiiiiiick?"
+    };
+    var validProperty = false;
+    var answerPrompt;
+
+    while(validProperty == false){
+        answerPrompt = prompt("Guess an Object Property!");
+        if(obj.hasOwnProperty(answerPrompt)){
+            alert(obj[answerPrompt]);
+            validProperty = true;
+        } else alert("Not a property, try again");
+
+    }
+    
+}
+
 Object1();
 Object2();
+Object3();
+FunObject();
